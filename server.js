ccd7e05910cf3b3f51825505773bdea725597ca6
Jim Motta <jim.motta@gmail.com>
2015-11-19 12:56:38 -0800

    Added middleware.js

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleWare = {
-	requireAuthentication: function (req, res, next) {
-		console.log('Private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log('Request (' + new Date().toString() + '): ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleWare = require('./middleware.js');
+
+// var middleWare = {
+// 	requireAuthentication: function (req, res, next) {
+// 		console.log('Private route hit!');
+// 		next();
+// 	},
+// 	logger: function (req, res, next) {
+// 		console.log('Request (' + new Date().toString() + '): ' + req.method + ' ' + req.originalUrl);
+// 		next();
+// 	}
+// };
 
 app.use(middleWare.logger);
 
